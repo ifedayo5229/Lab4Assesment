6ac00a21fd5366601b5f6d5a68a0c1470d236ef0
Ademodi Seun <seun@zagadat.onmicrosoft.com>
2021-04-30 21:56:19 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            int[] scores = new int[5];
+            Console.WriteLine("Welcome to my Cgpa & Cgpa percentage Calculator");
+
+
+          
+            Console.WriteLine("Enter first grade: ");
+            int firstscore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second grade: ");
+            int secondscore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third grade: ");
+            int thirdscore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter fourth grade: ");
+            int fourthscore = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter fifth grade: ");
+            int fifthscore = int.Parse(Console.ReadLine());
+
+            scores[0] = firstscore/10;
+            scores[1] = secondscore/10;
+            scores[2] = thirdscore/10;
+            scores[3] = fourthscore/10;
+            scores[4] = fifthscore/10;
+
+            int sum = 0;
+            double cgpa = 0;
+
+           
+            foreach (var score in scores)
+            {
+                
+                sum += score;
+                cgpa = sum / scores.Length;
+                   
+
+            }
+            Console.WriteLine("Your cgpa : " + cgpa);
+             Console.WriteLine("Your cgpa percentage  : " + cgpa*9.5 + "%");
+        }
+
+
+    }
+
+
+}
+
+
